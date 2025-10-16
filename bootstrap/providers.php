07eb39de7100e7495a8bbf84e45fc76b0817555d
Apri Pandu Wicaksono <sarahpalastrin@gmail.com>
2025-10-16 10:38:50 +0700

    chore(bootstrap): update provider list

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -3,6 +3,5 @@
 return [
     App\Providers\AppServiceProvider::class,
     App\Providers\BindServiceProvider::class,
-    App\Providers\GateServiceProvider::class,
     App\Providers\RouteServiceProvider::class,
 ];
